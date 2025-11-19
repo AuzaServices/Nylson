10f671d1ad72d43a1ca461716e558cb7c8a57117
Davi <andrielysom@gmail.com>
2025-11-19 01:27:01 -0300

    vai

--- public/painel.js
+++ public/painel.js
@@ -19,7 +19,7 @@ async function carregarCadastros() {
       <td><a href="${c.selfieDoc}" target="_blank">Abrir</a></td>
 <td>
   ${c.fotoCamera
-    ? `<img src="${c.fotoCamera.startsWith('data:image') ? c.fotoCamera : `data:image/png;base64,${c.fotoCamera}`}" width="100" height="80" alt="Foto da câmera" />`
+    ? `<img src="${c.fotoCamera}" width="100" height="80" alt="Foto da câmera" />`
     : "-"}
 </td>
     `;
